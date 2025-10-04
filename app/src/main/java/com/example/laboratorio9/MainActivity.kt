c2b86989b537709d8534f39576c9f01d1f4711e2
joel55p <joelnerio42@gmail.com>
2025-10-03 22:39:24 -0600

    prueba

--- app/src/main/java/com/example/laboratorio9/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio9/MainActivity.kt
@@ -1,11 +1,16 @@
-// MainActivity.kt
 package com.example.laboratorio9
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.laboratorio9.ui.theme.LikeCounterScreen
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.laboratorio9.ui.theme.Laboratorio9Theme
 
 class MainActivity : ComponentActivity() {
@@ -14,8 +19,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Laboratorio9Theme {
-                LikeCounterScreen()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Laboratorio9Theme {
+        Greeting("Android")
     }
 }
